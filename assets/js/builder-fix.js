45d38dd0f43a21b7661df472f28c82907be385a2
Onur Yüksektepeli <onuryuksektepeli@hotmail.com.tr>
2021-06-12 12:44:42 +0300

    Initial commit

---
+++ assets/js/builder-fix.js
@@ -0,0 +1,15 @@
+Startuply.hidePreloader = function (callback) {
+    var _this = this;
+
+    if ( callback ) {
+        callback();
+    }
+}
+
+Startuply.flexSliderInit = function () {
+    return;
+}
+
+Startuply.videoBackgroundInit = function () {
+    return;
+}
